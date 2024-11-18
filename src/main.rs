bd81eb3bc195f7bacc1922564db73abfdee61aed
Umar_as_suguri <omar@sogratl.com>
2024-11-18 12:51:28 +0300

    Пока только получение блоков и закение времени до получения слота

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,8 @@ use tungstenite::Message::Text;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // ... ваша конфигурация RPC
-    let window = Window::new(1000000);
-    let rpc_url = "https://api.mainnet-beta.solana.com";
-    let client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed());
+
+    let client = RpcClient::new("https://api.devnet.solana.com");
 
     let config = RpcBlockConfig {
         max_supported_transaction_version: Some(0),
@@ -27,50 +26,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // Получаем информацию о последнем блоке
         let local_time: DateTime<Local> = Local::now();
-
-        // Форматируем время в нужном формате
         let formatted_time = local_time.format("%H:%M:%S:%f").to_string();
-
         println!("До запроса слота, время: {}", formatted_time);
         let slot = match client.get_slot() {
             Ok(s) => s,
+
             Err(e) => {
                 println!("Failed to get slot: {}", e);
                 continue; // Retry after 1 second
             }
         };
-        let local_time: DateTime<Local> = Local::now();
-
-        // Форматируем время в нужном формате
-        let formatted_time = local_time.format("%H:%M:%S:%f").to_string();
-        println!("После запроса слота, время: {}", formatted_time);
-
-
-    }
-}
-/*
-
-#[tokio::main]
-async fn main() {
-
-
-    loop {
-        let slot = match client.get_slot() {
-            Ok(s) => s,
-            Err(e) => {
-                println!("Failed to get slot: {}", e);
-                sleep(Duration::from_millis(100)).await;
-                continue; // Retry after 1 second
-            }
-        };
-
-        let config = RpcBlockConfig {
-            max_supported_transaction_version: Some(0),
-            ..Default::default()
-        };
-
-        sleep(Duration::from_millis(100)).await;
-        let mut time = client.get_block_time(slot);
         // Получаем блок по слоту
         match client.get_block_with_config(slot, config) {
             Ok(block) => {
@@ -97,8 +62,10 @@ async fn main() {
                 println!("Failed to get block: {}", e);
             }
         }
+        let local_time: DateTime<Local> = Local::now();
+        let formatted_time = local_time.format("%H:%M:%S:%f").to_string();
+        println!("После запроса слота, время: {}", formatted_time);
 
-        // Пауза перед следующей итерацией
-        sleep(Duration::from_millis(1000)).await;
+
     }
-}*/
+}
